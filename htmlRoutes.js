19e86a41b389c7b28e9cf456951f5b2086fdefa9
Trillville 💯 <haydenmejia@gmail.com>
2018-07-12 19:12:29 -0600

    ok

--- htmlRoutes.js
+++ htmlRoutes.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = function(app){
 
 	app.get('/survey', function(req, res){
-		res.sendFile(path.join(__dirname + './survey.html'));
+		res.sendFile(path.join(__dirname, './survey.html'));
 	});
 
 	app.use(function(req, res){
